83a7e139bf5901738d2fafa50d9e580a1e688a5e
raych78 <rayhanch28@gmail.com>
2026-04-26 18:17:07 +0200

    created  versions.tf

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-    required_version = ">= 1.3.0"
+    required_version = ">= 1.5.0"
 
     required_providers {
         aws = {
-            source = 
-            version =
+            source = "hashicorp/aws"
+            version = ">= 5.0.0"
         }
     }
 
